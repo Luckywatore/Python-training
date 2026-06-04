c91755df32a1e1e3eb557c334058c34935bb7900
Lucky Watore <luckywatore>
2026-06-04 12:15:33 +0530

    First program

---
+++ cal.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter number 1 "))
+num2 = int(input("Enter number 2 "))
+
+print(f"The sum of {num1} and {num2} is:{num1 + num2} ")
+print(f"The sub of {num1} and {num2} is:{num1 - num2} ")
+print(f"The mul of {num1} and {num2} is:{num1 * num2} ")
+print(f"The div of {num1} and {num2} is:{num1 / num2} ")
+
